2095c3e59af85231c770bd912da891524ac2ecce
draffe <draffe@mvfactory.com>
2023-12-02 12:33:35 -0300

    sasdasd

---
+++ EjemploTests/PruebasdeMetodos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploTests
+{
+    public class PruebasdeMetodos
+    {
+
+         public int NumeroMayor(int numeroUno, int NumeroDos )
+        {
+
+            if (numeroUno>NumeroDos)
+                return numeroUno; 
+            else 
+                return NumeroDos;
+
+        }
+
+    }
+}
---
+++ Modulos.Avanzado.Test/Testbasico.cs
@@ -0,0 +1,62 @@
+using EjemploTests;
+using Microsoft.EntityFrameworkCore;
+using Modulos.Avanzado.Sabados.Dominio;
+using Modulos.Avanzado.Sabados.Repositorio;
+using Modulos.Avanzado.Sabados.Servicios;
+using Modulos.Avanzados.Sabados.DB;
+
+namespace Modulos.Avanzado.Test
+{
+    [TestClass]
+    public class Testbasico
+    {
+
+
+
+
+        [TestMethod]
+        public void  TestUno()
+        {
+
+            int numerouno = 9;
+            int numerodos = 10;
+            var ejemplo =  new PruebasdeMetodos();
+
+            var resul = ejemplo.NumeroMayor(numerouno, numerodos);
+            Assert.AreEqual(10, resul);
+        }
+
+
+        [TestMethod]
+        public async Task AddClienteTest()
+        {
+           var options = new DbContextOptionsBuilder<ModulosAvanzadoDBContext>()
+                .UseInMemoryDatabase(databaseName: $"PruebasSabnados-{Guid.NewGuid()}")
+                .Options;
+            
+            IAsyncBaseRepositorio<Cliente> asyncBaseRepositorio = new BaseRepositorio<Cliente>(new ModulosAvanzadoDBContext(options));
+            ServicioCLiente servicioCLiente = new ServicioCLiente(asyncBaseRepositorio);
+         var reult1 =   await servicioCLiente.AddCliente(new Cliente { 
+              Apellido = "asdasd",
+              Correo ="dixon1002@gmail.com",
+              FechadeNacimiento = DateTime.Now,
+              Nombre ="asdas",
+              Password ="asdasd",
+              
+            });
+            Assert.AreEqual(true, reult1);
+            var resul =  await servicioCLiente.AddCliente(new Cliente
+            {
+                Apellido = "asdasd",
+                Correo = "dixon1002@gmail.com",
+                FechadeNacimiento = DateTime.Now,
+                Nombre = "asdas",
+                Password = "asdasd",
+
+            });      
+
+            Assert.AreEqual(false, resul);
+
+        }
+    }
+}
